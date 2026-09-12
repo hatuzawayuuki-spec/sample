3de1318a75e761589ad00270dfedad53f795dcaf
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2026-09-12 15:13:25 +0900

    Add Tailwind CSS support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hatuzawayuuki-spec.github.io',
   base: '/sample',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
